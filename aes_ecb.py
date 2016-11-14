54eb6c99d572fe4539d8ac2da47d5ff33d0be800
hasherezade <hasherezade@op.pl>
2016-11-14 21:11:46 +0100

    [FEATURE] AES ECB read key from file

--- aes_ecb.py
+++ aes_ecb.py
@@ -9,13 +9,13 @@ import getpass
 from Crypto.Cipher import AES
 from Crypto import Random
 
-BS = 16
+BS = 32
 pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS) 
 unpad = lambda s : s[:-ord(s[len(s)-1:])]
 
 class AESCipher:
     def __init__( self, key ):
-        self.key = key
+        self.key = "".join(map(chr, key))
 
     def encrypt( self, raw ):
         raw = pad(raw)
@@ -48,11 +48,23 @@ def main():
     parser.add_argument('--infile', dest="infile", default=None, help="Input file")
     parser.add_argument('--outfile', dest="outfile", default="out.tmp", help="Output file")
     parser.add_argument('--key', dest="key", default="test", help="Key")
+    parser.add_argument('--keyfile', dest="keyfile", default=None, help="File with the key")
     parser.add_argument('--decode', dest="decode", default=False, action='store_true', help="Decode or encode the given input?")
     args = parser.parse_args()
-    
-    key = expand_key(args.key)
+
+
+    if (args.key == None and args.keyfile == None):
+        print "Supply key or keyfile"
+        exit (-1)
+
+    if args.keyfile:
+        key = bytearray(open(args.keyfile, 'rb').read())
+    else:
+        key = bytearray(args.key)
+
+    key = expand_key(key)
     print key
+    print len(key)
 
     if args.infile is None:
         #read message from stdin:
